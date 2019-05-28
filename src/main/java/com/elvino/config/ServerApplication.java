fc64f24d3b0d880e2a5af63f266268a8993a3444
elvino <elvino_tan@yahoo.com>
2019-05-28 23:24:55 +0700

    Spring Cloud Config Example

---
+++ src/main/java/com/elvino/config/ServerApplication.java
@@ -0,0 +1,15 @@
+package com.elvino.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
